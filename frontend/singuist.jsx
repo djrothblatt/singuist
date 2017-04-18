7b5165bc0e78d7554896fcd23a232636b27764cb
Daniel Rothblatt <djrothblatt@gmail.com>
2017-04-18 17:49:53 -0400

    Create React/Redux store

--- frontend/singuist.jsx
+++ frontend/singuist.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
     ReactDOM.render(<h1>Introduction to Singuistics</h1>, root);
 });
