f5cea86142568b59ad397d0802de1b1cf2eaf655
Daniel Rothblatt <djrothblatt@gmail.com>
2017-04-27 17:43:41 -0400

    Add circles around top 3 index items

--- frontend/components/tracks/index/tracks_index_item.jsx
+++ frontend/components/tracks/index/tracks_index_item.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function TracksIndexItem({ track, index }) {
+    const classes = index < 3 ? 'index-counter top-three' : 'index-counter';
     return (
 	<Link to={`/tracks/${track.id}/`}>
 	  <li className="track-index-item">
-	    <h2 className="index-counter">{index + 1}</h2>
+	    <h2 className={classes}>{index + 1}</h2>
 	    <div className="info-container">
 	      <h1>{track.name}</h1>
 	      <h3>{track.artist}</h3>
