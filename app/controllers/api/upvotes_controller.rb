5764ab38a1f1849e4908c0f647fbf39eba1865b1
Daniel Rothblatt <djrothblatt@gmail.com>
2017-05-10 15:43:11 -0400

    Create, destroy upvotes

--- app/controllers/api/upvotes_controller.rb
+++ app/controllers/api/upvotes_controller.rb
@@ -1,3 +1,24 @@
 class Api::UpvotesController < ApplicationController
-  
+  def create
+    @upvote = Upvote.new(upvote_params)
+
+    if @upvote.save
+      render json: @upvote.annotation.id
+    else
+      render json: @upvote.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    annotation_id = @upvote.annotation.id
+    @upvote = Upvote.find(params[:id])
+    @upvote.destroy
+    render json: annotation_id
+  end
+
+  private
+
+  def upvote_params
+    params.require(:upvote).permit([:annotation_id, :user_id])
+  end
 end
