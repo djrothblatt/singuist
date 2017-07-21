2b5204d1b1c58bbdb9ceabb541a774da202a99b3
Daniel Rothblatt <djrothblatt@gmail.com>
2017-07-21 14:16:13 -0400

    Correct logic for keeping Redux logger out of production

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,7 +4,7 @@ import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 let middlewares = [thunk];
-if (process.env.NODE_ENV !== 'development') {
+if (process.env.NODE_ENV !== 'production') {
     middlewares = [...middlewares, logger];
 }
 
